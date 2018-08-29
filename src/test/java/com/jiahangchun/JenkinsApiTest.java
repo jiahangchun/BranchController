0c90a160291978c8d6a3b6496070bc3ba20b3bad
jiahangchun <jiahangchun@mockuai.com>
2018-08-29 08:46:03 +0800

    jenkinsRemoteApi调试通过

---
+++ src/test/java/com/jiahangchun/JenkinsApiTest.java
@@ -0,0 +1,112 @@
+package com.jiahangchun;
+
+import com.offbytwo.jenkins.JenkinsServer;
+import com.offbytwo.jenkins.helper.BuildConsoleStreamListener;
+import com.offbytwo.jenkins.model.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author chunchun
+ * @descritpion
+ * @date Created at 2018/8/28 上午9:15
+ **/
+public class JenkinsApiTest {
+
+    private static JenkinsServer jenkins = null;
+
+    static {
+        try {
+            jenkins = new JenkinsServer(new URI("http://localhost:8080"), "test", "test");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        JenkinsApiTest jenkinsApiTest = new JenkinsApiTest();
+//        jenkinsApiTest.startJob();
+
+//        jenkinsApiTest.printlog();
+        jenkinsApiTest.createJob();
+    }
+
+    /**
+     * 开始任务
+     *
+     * @throws IOException
+     */
+    private void startJob() throws IOException {
+        Job job = jenkins.getJob("test");
+        job.build(true);//没参数的启动方式
+//        job.build(new HashMap<>(),true);//有参数的启动方式
+    }
+
+    /**
+     * 删除任务
+     * @throws IOException
+     */
+    private void deleteJob() throws IOException {
+        jenkins.deleteJob("test",true);
+    }
+
+    private void createJob() throws IOException {
+        String jobXml =jenkins.getJobXml("test");
+        System.out.println(jobXml);
+        jenkins.createJob("test2",jobXml,true);
+    }
+
+    /**
+     * 启动记录
+     * @throws IOException
+     */
+    private void getBuilds() throws IOException {
+        List<Build> list= jenkins.getJob("test").getAllBuilds();
+        for(Build build:list){
+            System.out.println(build.details().getConsoleOutputText());
+        }
+    }
+
+    /**
+     * 打印日志
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private void printlog() throws IOException, InterruptedException {
+        Job job = jenkins.getJob("test");
+        BuildWithDetails build =((JobWithDetails) job).getLastBuild().details();
+        // Get log with initial offset
+//        int offset = 40;
+//        String output = build.getConsoleOutputText(offset).getConsoleLog();
+        // Stream logs (when build is in progress)
+        BuildConsoleStreamListener buildListener = new PartiallyGetConsole();
+        build.streamConsoleOutput(buildListener, 1, 420);
+
+
+        //还是那个crumb问题
+        //估计写的这个人一开始就没打算用crumb=true的方式，希望用手动设置的方式设置jenkins.可以从issus中看出
+//        Build lastCompletedBuild = job.details().getLastCompletedBuild();
+//        TestReport testReport = lastCompletedBuild.getTestReport();
+//        System.out.println(testReport.getTotalCount());
+    }
+
+
+    public class PartiallyGetConsole implements BuildConsoleStreamListener{
+
+        @Override
+        public void onData(String s) {
+            System.out.println(s);
+        }
+
+        @Override
+        public void finished() {
+            System.out.println("finished");
+        }
+    }
+
+}
--- src/test/java/com/jiahangchun/JenkinsRemoteApiTest.java
+++ src/test/java/com/jiahangchun/JenkinsRemoteApiTest.java
@@ -1,22 +1,24 @@
-package com.jiahangchun;
-
-import com.cdancy.jenkins.rest.JenkinsClient;
-import com.cdancy.jenkins.rest.domain.system.SystemInfo;
-
-/**
- * @author chunchun
- * @descritpion
- * @date Created at 2018/8/27 下午3:30
- **/
-public class JenkinsRemoteApiTest {
-
-    public static void main(String[] args) {
-        JenkinsClient client = JenkinsClient.builder()
-                .endPoint("http://127.0.0.1:8080") // Optional. Defaults to http://127.0.0.1:8080
-                .credentials("admin:password") // Optional.
-                .build();
-
-        SystemInfo systemInfo = client.api().systemApi().systemInfo();
-        System.out.println(systemInfo.jenkinsVersion());
-    }
-}
+//package com.jiahangchun;
+//
+//import com.cdancy.jenkins.rest.JenkinsClient;
+//import com.cdancy.jenkins.rest.domain.system.SystemInfo;
+//import com.google.common.collect.Maps;
+///**
+// * @author chunchun
+// * @descritpion
+// * @date Created at 2018/8/27 下午3:30
+// **/
+//public class JenkinsRemoteApiTest {
+//
+//    public static void main(String[] args) {
+//        JenkinsClient client = JenkinsClient.builder()
+//                .endPoint("http://127.0.0.1:8080") // Optional. Defaults to http://127.0.0.1:8080
+////                .credentials("test:test") // Optional.
+//                .build();
+//
+//        SystemInfo systemInfo = client.api().systemApi().systemInfo();
+//        System.out.println(systemInfo.jenkinsVersion());
+//
+//
+//    }
+//}
