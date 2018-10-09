708177fc49a52ba50887585057ebb9a7fcd292a7
jiahangchun <jiahangchun@mockuai.com>
2018-10-09 10:51:28 +0800

    AnnotationBeanDefinitionReader ： @Commponent的解析

--- src/main/java/com/jiahangchun/service/beanPostProcessor/TestBeanPostProcessor.java
+++ src/main/java/com/jiahangchun/service/beanPostProcessor/TestBeanPostProcessor.java
@@ -13,13 +13,13 @@ import org.springframework.stereotype.Component;
 public class TestBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("TestBeanPostProcessor.postProcessBeforeInitialization");
+//        System.out.println("TestBeanPostProcessor.postProcessBeforeInitialization");
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("TestBeanPostProcessor.postProcessBeforeInitialization");
+//        System.out.println("TestBeanPostProcessor.postProcessBeforeInitialization");
         return bean;
     }
 }
