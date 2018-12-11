c5dd1df82aeccd3e010c5950183460c47dda8959
jiahangchun <jiahangchun@mockuai.com>
2018-12-11 14:23:06 +0800

    dd

--- src/test/java/com/jiahangchun/Demo.java
+++ src/test/java/com/jiahangchun/Demo.java
@@ -30,7 +30,7 @@ public class Demo {
 //            Counter.increment();
 //            System.out.println("counter: " + Counter.value());
 //            TimeUnit.SECONDS.sleep(1);
-//        }
+//        }dd
 //    }
 
     public static void main(String[] args) {
