e6e48f0c8a8e2354a7a2089e9dceb100c065cc9f
jiahangchun <jia123456>
2019-03-05 07:11:18 -0800

    。。。

--- src/main/java/com/jiahangchun/tool/StartController.java
+++ src/main/java/com/jiahangchun/tool/StartController.java
@@ -1,23 +1,23 @@
-package com.jiahangchun.tool;
-
-import com.jiahangchun.test.ExampleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author chunchun
- * @descritpion
- * @date Created at 2019/1/11 下午3:29
- **/
-@RestController
-public class StartController {
-
-    @Autowired
-    public ExampleService exampleService;
-
-    @GetMapping("/input")
-    public String input(String word){
-        return exampleService.wrap(word);
-    }
-}
+//package com.jiahangchun.tool;
+//
+//import com.jiahangchun.test.ExampleService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author chunchun
+// * @descritpion
+// * @date Created at 2019/1/11 下午3:29
+// **/
+//@RestController
+//public class StartController {
+//
+//    @Autowired
+//    public ExampleService exampleService;
+//
+//    @GetMapping("/input")
+//    public String input(String word){
+//        return exampleService.wrap(word);
+//    }
+//}
